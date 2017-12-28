c01ad54942355654290fcd32caa7340963b915e0
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-28 04:53:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < list.length; i++){
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + parseInt(n)).toString()
+    }
+}
+
+function deepestChild() {
+  var nodeArray = []
+  var ContainerList = document.querySelector('#grand-node div div div div')
+  return ContainerList
+}
